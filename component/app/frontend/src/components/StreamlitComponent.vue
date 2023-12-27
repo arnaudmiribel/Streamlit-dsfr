cc6217dc5697d034473dfad0ea39ccd91d9e40db
Matiboux <matiboux@gmail.com>
2023-12-27 22:02:43 +0100

    Add StreamlitComponent to refactor

---
+++ component/app/frontend/src/components/StreamlitComponent.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { Component } from 'vue'
+
+import WithStreamlitConnection from './streamlit/WithStreamlitConnection.vue'
+
+const props = defineProps<{
+	component: Component
+}>()
+</script>
+
+<template>
+	<WithStreamlitConnection v-slot="{ width, args, disabled, theme }">
+		<props.component :width="width" :args="args" :disabled="disabled" :theme="theme" />
+	</WithStreamlitConnection>
+</template>

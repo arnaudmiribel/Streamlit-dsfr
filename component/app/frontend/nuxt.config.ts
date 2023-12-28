52b997f0606059b0d354fd962305e3bf1172c068
Matiboux <matiboux@gmail.com>
2023-12-28 02:27:02 +0100

    Include dsfr css styles

--- component/app/frontend/nuxt.config.ts
+++ component/app/frontend/nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	srcDir: './src',
+	css: [
+		'@gouvfr/dsfr/dist/dsfr.min.css',
+		'@gouvminint/vue-dsfr/styles',
+	],
   	devtools: {
 		enabled: false,
 	},

3c148404c69ebf75761c88eed26bd42270f1261b
Matiboux <matiboux@gmail.com>
2023-12-28 22:35:41 +0100

    Fix hydration mismatch error

---
+++ component/frontend/src/views/MyComponent.vue
@@ -0,0 +1,8 @@
+<script setup>
+import StreamlitComponent from '~/components/StreamlitComponent.vue'
+import Component from '~/components/MyComponent.vue'
+</script>
+
+<template>
+	<StreamlitComponent :component="Component" client:load />
+</template>

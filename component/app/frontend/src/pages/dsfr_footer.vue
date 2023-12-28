5a3c234490702b253b9039bb4ed6fc9076c3e522
Matiboux <matiboux@gmail.com>
2023-12-28 02:40:36 +0100

    Add DsfrFooter component

---
+++ component/app/frontend/src/pages/dsfr_footer.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import StreamlitComponent from '../components/StreamlitComponent.vue'
+import DsfrFooter from '../components/DsfrFooter.vue'
+</script>
+
+<template>
+	<StreamlitComponent :component="DsfrFooter" />
+</template>

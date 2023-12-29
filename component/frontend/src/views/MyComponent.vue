7098a60840ec16879841c43e007e474bf4e2265e
Matiboux <matiboux@gmail.com>
2023-12-29 17:04:12 +0100

    Remove my_component

--- component/frontend/src/views/MyComponent.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import StreamlitComponent from '~/components/StreamlitComponent.vue'
-import Component from '~/components/MyComponent.vue'
-</script>
-
-<template>
-	<StreamlitComponent :component="Component" client:load />
-</template>

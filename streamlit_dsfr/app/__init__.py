0b2bc6dd4f03b61b1bbf2ae58241b19f25dc20e5
Mati <mathieu.guerin@saegus.com>
2024-01-03 15:14:32 +0100

    Fix components declaration & routes (#3)
    * Refactor components declaration
    
    * Remove my_component declaration
    
    * Fix route to dsfr button component
    
    * Update components wrapper definition
    
    * Revert "Update components wrapper definition"
    
    This reverts commit 3cda9daa040c0d85cd4092d5e9d3e7dfdb17564b.
    
    * Add comment above wrapper functions

--- streamlit_dsfr/app/__init__.py
+++ streamlit_dsfr/app/__init__.py
@@ -18,69 +18,35 @@ _RELEASE = (os.environ.get('APP_ENV') or 'prod') == 'prod'
 # your component frontend. Everything else we do in this file is simply a
 # best practice.
 
+supported_components = {
+    'dsfr_button': 'st_dsfr_button',
+}
+
 if not _RELEASE:
+    # When components are in development, we use `url` to tell Streamlit
+    # that the component will be served by a local dev server.
     components_url = 'http://localhost:8000'
-    _my_component_func = components.declare_component(
-        # We give the component a simple, descriptive name ("my_component"
-        # does not fit this bill, so please choose something better for your
-        # own component :)
-        'my_component',
-        # Pass `url` here to tell Streamlit that the component will be served
-        # by the local dev server that you run via `npm run start`.
-        # (This is useful while your component is in development.)
-        url = components_url + '/my_component'
-    )
-    _dsfr_button_func = components.declare_component(
-        'dsfr_button',
-        url = components_url + '/dsfr_button'
-    )
+    for component in supported_components:
+        globals()[f'_{component}_func'] = \
+            components.declare_component(
+                component,
+                url = f'{components_url}/{supported_components[component]}',
+            )
 else:
-    # When we're distributing a production version of the component, we'll
-    # replace the `url` param with `path`, and point it to the component's
-    # build directory:
+    # When we are distributing a production version of the component, we
+    # use `path` instead of `url`. This tells Streamlit to load the component
+    # from the component build directory directly.
     parent_dir = os.path.dirname(os.path.abspath(__file__))
     build_dir = os.path.join(parent_dir, 'frontend')
-    _my_component_func = components.declare_component('my_component', path = os.path.join(build_dir, 'my_component'))
-    _dsfr_button_func = components.declare_component('dsfr_button', path = os.path.join(build_dir, 'dsfr_button'))
+    for component in supported_components:
+        globals()[f'_{component}_func'] = \
+            components.declare_component(
+                component,
+                path = os.path.join(build_dir, supported_components[component]),
+            )
 
 
-# Create a wrapper function for the component. This is an optional
-# best practice - we could simply expose the component function returned by
-# `declare_component` and call it done. The wrapper allows us to customize
-# our component's API: we can pre-process its input args, post-process its
-# output value, and add a docstring for users.
-def my_component(name, key = None):
-    """Create a new instance of "my_component".
-
-    Parameters
-    ----------
-    name: str
-        The name of the thing we're saying hello to. The component will display
-        the text "Hello, {name}!"
-    key: str or None
-        An optional key that uniquely identifies this component. If this is
-        None, and the component's arguments are changed, the component will
-        be re-mounted in the Streamlit frontend and lose its current state.
-
-    Returns
-    -------
-    int
-        The number of times the component's "Click Me" button has been clicked.
-        (This is the value passed to `Streamlit.setComponentValue` on the
-        frontend.)
-
-    """
-    # Call through to our private component function. Arguments we pass here
-    # will be sent to the frontend, where they'll be available in an "args"
-    # dictionary.
-    #
-    # "default" is a special argument that specifies the initial return
-    # value of the component before the user has interacted with it.
-    component_value = _my_component_func(name = name, key = key, default = 0)
-
-    # We could modify the value returned from the component if we wanted.
-    # There's no need to do this in our simple example - but it's an option.
-    return component_value
+# Components wrapper functions for users
 
 def dsfr_button(label, key = None):
 	component_value = _dsfr_button_func(label = label, key = key, default = False)
